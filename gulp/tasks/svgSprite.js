eb5653456ff48a28fc41a0c0897286c1a5b3b947
nikkojustix <nickolaykotov@gmail.com>
2022-07-03 23:03:16 +0300

    initial commit

---
+++ gulp/tasks/svgSprite.js
@@ -0,0 +1,25 @@
+import SVGSprite from 'gulp-svg-sprite';
+
+export const svgSprite = () => {
+  return app.gulp
+    .src(app.path.src.svgicons)
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: 'SVG',
+          message: 'error: <%=error.message %>',
+        })
+      )
+    )
+    .pipe(
+      SVGSprite({
+        mode: {
+          stack: {
+            sprite: `../icons/icons.svg`,
+            example: true,
+          },
+        },
+      })
+    )
+    .pipe(app.gulp.dest(app.path.build.images));
+};
