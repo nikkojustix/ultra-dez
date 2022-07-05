f6757102bf58ce146426175b67291c70fe33737b
nikkojustix <nickolaykotov@gmail.com>
2022-07-06 01:00:12 +0300

    added sticky header

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 import * as flsFunctions from './modules/functions.js';
 
 flsFunctions.isWebp();
+
+const sticky = document.querySelector('.header__sticky');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 300) {
+    console.log(window.pageYOffset);
+    sticky.classList.add('header__sticky--visible');
+  } else {
+    sticky.classList.remove('header__sticky--visible');
+  }
+});
+
+const modal = new GraphModal();
